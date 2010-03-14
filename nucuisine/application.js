d0ad3a414c541cdbd1b3cf11ba2c2d20c3fa0458
Christopher E. Walker <cewalker@uchicago>
2010-03-14 03:11:13 -0500

    application optimized for minification

--- nucuisine/application.js
+++ nucuisine/application.js
@@ -1,15 +1,23 @@
-var isTouch = false;
-if ((typeof window.Touch)==='object' ) {
-	isTouch = true;
-}
+
+aData = {};
+(function(){
+
+var
+	// constants:
+	IS_TOUCH = !!window.Touch,
+	EVENT_KIND = IS_TOUCH ? 'tap' : 'click',
+	IS_IPHONE  = navigator.userAgent.indexOf( 'iPhone' ) > -1,
+	
+	// local variables:
+	type, hash, i;
 
 $.jQTouch({
     icon: 'kilo.png',
     statusbar: 'black-translucent',
-	useAnimations: true, 
+	useAnimations: IS_IPHONE, 
 	slideSelector: 'li a',
 	flipSelector: '#meal li a',
-	useFastTouch: isTouch,
+	useFastTouch: IS_TOUCH,
 	initializeTouch: 'a',
 	preloadImages: [
 		'libs/jqtouch/themes/apple/img/backButtonPurple.png'
@@ -19,9 +27,10 @@ $.jQTouch({
 $(document).ready( function() {
 
 	var numenu = new Numenu({
-		data: eatins,
-		type: 'places'
-	});    
+		data: eatins, // eatins IS A GLOBAL VARIABLE!
+		type: 'places',
+		nutridata: aData
+	});
 	// console.log( numenu.data );
 
 	$('#places').html( numenu.makeAll() );
@@ -29,27 +38,35 @@ $(document).ready( function() {
 	function getGuts(n) {   
 		// console.log('adding junk');
 		
-		if (!n) {alert('probl'); return;}
+		//if (!n) {alert('probl'); return;}
 		// console.log( n );
 		// console.log( n.rel, n.hash );
-		if (n.hash==='#venue') {
-			numenu.type = 'venue';
-			numenu.venue = n.rel;
-		}
-		if (n.hash==='#day') {
-			numenu.type = 'day';
-			numenu.day = n.rel;
-		}
-		if (n.hash==='#meal') {
-			numenu.type = 'meal';
-			numenu.meal = n.rel;
-		}
-		if (n.hash==='#nutrid') {
-			numenu.type = 'nutrition';
-			numenu.item = n.innerHTML;
-			numenu.nutrilink = n.rel;   
-		}     
-		$( n.hash ).html(numenu.makeAll());      
+		
+		type = numenu.type;
+		hash = n.hash;
+		
+		switch (hash) {
+			case '#venue':
+				type = 'venue';
+				numenu.venue = n.rel;
+				break;
+			case '#day':
+				type = 'day';
+				numenu.day = n.rel;
+				break;
+			case '#meal':
+				type = 'meal';
+				numenu.meal = n.rel;
+				break;
+			case '#nutrid':
+				type = 'nutrition';
+				numenu.item = n.innerHTML;
+				numenu.nutrilink = n.rel;
+				break;
+		}		
+		numenu.type = type;
+		    
+		$( hash ).html(numenu.makeAll());      
 	}
 	
 	$( 'body' ).bind( 'pageAnimationStart', function(e,info) {
@@ -69,9 +86,8 @@ $(document).ready( function() {
 	// 	}
 	// });
 	
-	var eventKind = isTouch ? 'tap' : 'click';
 	
-	$('body > div').delegate('a', eventKind, function(e) {  
+	$('body > div').delegate('a', EVENT_KIND, function(e) {  
 		//alert('tapped');
 		// console.log( 'delegated', e, this );
 		// $('body').data( 'activator', this ); 
@@ -79,101 +95,4 @@ $(document).ready( function() {
 	});  
 });
 
-var subbing_template, sub, i, re;
-function nf(idx) {
-		// console.log( idx );
-		// global nftextnew
-		subbing_template = nutrition_template;
-		sub = aData[idx];
-		for (i=0; i<25; i++) {
-			re = new RegExp( '~' + i + '~');
-			subbing_template = subbing_template.replace( re, sub[i] );
-		}
-		// $( '#nutrition' ).replaceWith( subbing_template ); 
-		return subbing_template;
-}
-
-aData=new Object(); 
-
-var nutrition_template = 
-'<div id="nutrition">' +
-	'<h1>Nutrition Facts</h1>' +
-	'<p>Serving Size ~0~</p>' +
-	'<table>' +
-		'<tbody>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Amount Per Serving</strong></td>' +
-				'<td></td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Calories</strong> ~1~</td>' +
-				'<td>Calories From Fat ~2~</td>' +
-			'</tr>' +
-		'</tbody>' +
-	'</table>' +
-	'<table>' +
-		'<tbody>' +
-			'<tr>' +
-				'<td colspan="3"><strong>% Daily Value*</strong></td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Total Fat</strong> ~3~g</td>' +
-				'<td>~4~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td></td>' +
-				'<td>Saturated Fat ~5~g</td>' +
-				'<td>~6~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td></td>' +
-				'<td><em>Trans</em> Fat ~7~</td>' +
-				'<td></td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Cholesterol</strong> ~8~mg</td>' +
-				'<td>~9~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Sodium</strong> ~10~mg</td>' +
-				'<td>~11~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Total Carbohydrates</strong> ~12~g</td>' +
-				'<td>~13~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td></td>' +
-				'<td>Dietary Fiber ~14~g</td>' +
-				'<td>~15~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td></td>' +
-				'<td>Sugars ~16~g</td>' +
-				'<td></td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td colspan="2"><strong>Protein</strong> ~17~g</td>' +
-				'<td></td>' +
-			'</tr>' +
-		'</tbody>' +
-	'</table>' +
-	'<table>' +
-		'<tbody>' +
-			'<tr>' +
-				'<td>Vitamin A ~18~%</td>' +
-				'<td>•</td>' +
-				'<td>Vitamin C ~19~%</td>' +
-			'</tr>' +
-			'<tr>' +
-				'<td>Calcium ~20~%</td>' +
-				'<td>•</td>' +
-				'<td>Iron ~21~%</td>' +
-			'</tr>' +
-		'</tbody>' +
-	'</table>' +
-	'<p>* Percent Daily Values are based on a 2,000 calorie diet.</p>' +
-	'<h2>~22~</h2>' +
-	'<p>~23~</p>' +
-	'<p>~24~</p>' +
-'</div>';
+})();
--- nucuisine/nu-menu.js
+++ nucuisine/nu-menu.js
@@ -1,7 +1,11 @@
-(function(){
+Eval = function(t) {
+	eval(t);
+};
+Numenu = (function(){
+	
 // ==============================
 
-var
+var 
 	// variables
 	daysOfWeek = ['monday','tuesday','wednesday','thursday','friday','saturday','sun_day'],
 	places     = ['hinman','allison','foster_east','foster_west','elder','sargent','willard','willies','tech'],
@@ -12,15 +16,96 @@ var
 		din: 'dinner',
 		lat: 'late night'
 	},
-	
 	// constants
 	CANNOT_LOAD_MESSAGE = 'Could not fetch data',
 	LOADING_CLASSNAME   = 'loading',
+	NUTRITION_TEMPLATE  = 
+		'<div id="nutrition">' +
+			'<h1>Nutrition Facts</h1>' +
+			'<p>Serving Size ~0~</p>' +
+			'<table>' +
+				'<tbody>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Amount Per Serving</strong></td>' +
+						'<td></td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Calories</strong> ~1~</td>' +
+						'<td>Calories From Fat ~2~</td>' +
+					'</tr>' +
+				'</tbody>' +
+			'</table>' +
+			'<table>' +
+				'<tbody>' +
+					'<tr>' +
+						'<td colspan="3"><strong>% Daily Value*</strong></td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Total Fat</strong> ~3~g</td>' +
+						'<td>~4~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td></td>' +
+						'<td>Saturated Fat ~5~g</td>' +
+						'<td>~6~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td></td>' +
+						'<td><em>Trans</em> Fat ~7~</td>' +
+						'<td></td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Cholesterol</strong> ~8~mg</td>' +
+						'<td>~9~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Sodium</strong> ~10~mg</td>' +
+						'<td>~11~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Total Carbohydrates</strong> ~12~g</td>' +
+						'<td>~13~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td></td>' +
+						'<td>Dietary Fiber ~14~g</td>' +
+						'<td>~15~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td></td>' +
+						'<td>Sugars ~16~g</td>' +
+						'<td></td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td colspan="2"><strong>Protein</strong> ~17~g</td>' +
+						'<td></td>' +
+					'</tr>' +
+				'</tbody>' +
+			'</table>' +
+			'<table>' +
+				'<tbody>' +
+					'<tr>' +
+						'<td>Vitamin A ~18~%</td>' +
+						'<td>•</td>' +
+						'<td>Vitamin C ~19~%</td>' +
+					'</tr>' +
+					'<tr>' +
+						'<td>Calcium ~20~%</td>' +
+						'<td>•</td>' +
+						'<td>Iron ~21~%</td>' +
+					'</tr>' +
+				'</tbody>' +
+			'</table>' +
+			'<p>* Percent Daily Values are based on a 2,000 calorie diet.</p>' +
+			'<h2>~22~</h2>' +
+			'<p>~23~</p>' +
+			'<p>~24~</p>' +
+		'</div>',
 	
 	// local variables
-	i;
+	p, NumenuClass, back, more, text, arrayOfReplacements, instance, items, i, l;
 
-Numenu = function( config ) {
+NumenuClass = function( config ) {
 	// places, venue, day, meal, nutrition, hours
 	this.type      = config.type; // places, venue, day, meal
 	this.venue     = config.venue;
@@ -30,133 +115,138 @@ Numenu = function( config ) {
 	this.activator = config.activator; // reference to node that was clicked before animation
 	this.item      = config.item;
 	this.nutrilink = config.nutrilink;
+	this.nutridata = config.nutridata;
 };
 
-var p = Numenu.prototype;
-p.makeHead = function() {
+p = NumenuClass.prototype;
 
-	var title = this.type,
-		back = '',
-		more = '';
-		
-	if  ( this.type === 'venue' ) {
-		title = this.venue;
-	}
-	if  ( this.type === 'nutrition' ) {
-		title = this.item;
-	}
-	if ( this.type === 'day' ) {
-		title = this.day;
-	}
-	if ( this.type === 'meal' ) {
-		title = meal_names[this.meal];
-	}
+
+p.makeAll = function() {
+	instance = this;
 	
-	if ( this.type !== 'places' ) {
-		back = '<a href="#" class="button back">Back</a>';
-	}
-	if ( this.type === 'places' ) {
-		more = '<h2>Current</h2>';
-	}
-		
-	return '<div class="toolbar nucuisine">' +
-		'<h1>' + title + '</h1>' + back +
-		'</div>' + more;
-};
-p.makeBody = function() {
-	var text = '';
-	if ( this.type === 'hours' ) {
-		text = 'hours dude';
-	}
-	
-	if ( this.type === 'venue' ) {
-		text +=  '<ul class="edgetoedge">';	
-		for ( day in this.data.d[this.venue] ) {
-			if (day !== 'hours' ) {
-				text += '<li class="arrow">';
-				text += '<a href="#day" rel="' + day + '">';
-				text += day;
-				text += '</a>';
-				text += '</li>';
-			}
-		}	
-		text += '</ul>';
-	}  
-	
-	if ( this.type === 'nutrition' ) {
-		text = "<p>No Nutritional Information</p>";
-		var nutrilink = this.nutrilink;
-		if ( aData[nutrilink] ) {
-			text = nf( nutrilink );
-		} else {
-			var instance = this;
-			$(instance.activator).addClass( LOADING_CLASSNAME );
-			// console.log('loading ajax');
-			$.ajax({
-				url: 'menus/' + this.venue + this.data.w + '.html',
-				async: false,
-				success: function(fullhtml) {
-					var nutriscript = fullhtml.match( /aData=new Object\(\);([\s\S]*?)<\/script>/ )[1];
-					eval( nutriscript );  
-					if ( aData[ instance.nutrilink ] ) {
-						text = nf( instance.nutrilink  );
-					}
-				},
-				error: function(r,s,e) {
-					// this should go somewhere else higher, in much above too!
-					alert( CANNOT_LOAD_MESSAGE );
-				}   
-			});
+	function makeHead() {
+
+		back = more = '';
+
+		if  ( instance.type === 'venue' ) {
+			text = instance.venue;
 		}
+		if  ( instance.type === 'nutrition' ) {
+			text = instance.item;
+		}
+		if ( instance.type === 'day' ) {
+			text = instance.day;
+		}
+		if ( instance.type === 'meal' ) {
+			text = meal_names[instance.meal];
+		}
+
+		if ( instance.type !== 'places' ) {
+			back = '<a href="#" class="button back">Back</a>';
+		}
+		if ( instance.type === 'places' ) {
+			more = '<h2>Current</h2>';
+		}
+
+		return '<div class="toolbar nucuisine">' +
+			'<h1>' + text + '</h1>' + back +
+			'</div>' + more;
 	}
-	
-	if ( this.type === 'places' ) {
-		text +=  '<ul class="edgetoedge">';	
-		for ( place in this.data.d ) {
-			text += '<li class="arrow">';
-			text += '<a href="#venue" rel="' + place + '">';
-			text += '<span>'+place+'</span>';
-			text += '</a>';
-			text += '</li>';
-		}	
-		text += '</ul>';
-	}
-	
-	if ( this.type === 'day' ) {
-		text +=  '<ul class="edgetoedge">';	
-		for ( meal in this.data.d[this.venue][this.day] ) {
-			text += '<li class="arrow">';
-			text += '<a href="#meal" rel="' + meal + '">';
-			text += meal_names[meal];
-			text += '</a>';
-			text += '</li>';
-		}	
-		text += '</ul>';
-	}
-	
-	if (this.type==='meal') {
-		text = '';
-		for ( station in this.data.d[this.venue][this.day][this.meal] ) {
-			text += '<h2>' + station + '</h2>';
-			var items = this.data.d[this.venue][this.day][this.meal][station];
-			text += '<ul class="edgetoedge">';
-			for ( var i = 0, l = items.length; i < l; i++ ) {
-				text += '<li class="arrow">';
-				text += '<a href="#nutrid" rel="' + items[i].n + '">';
-				text += items[i].i;
-				text += '</a>';
-				text += '</li>';
+	function makeBody() {
+
+		function makeNutritionTemplate( instance ) {
+			text = NUTRITION_TEMPLATE;
+			arrayOfReplacements = instance.nutridata[ instance.nutrilink ];
+			for (i=0; i<25; i++) {
+				text = text.replace(new RegExp('~'+i+'~'), arrayOfReplacements[i]);
 			}
+			return text;
+		}
+
+		text = '<ul class="edgetoedge">';
+		if ( instance.type === 'venue' ) {
+			for ( day in instance.data.d[instance.venue] ) {
+				if (day !== 'hours' ) {
+					text += '<li class="arrow">' +
+					        '<a href="#day" rel="' + day + '">' +
+							day +
+					        '</a>' +
+					        '</li>';
+				}
+			}	
+			text += '</ul>';
+		}  
+
+		if ( instance.type === 'places' ) {
+			for ( place in instance.data.d ) {
+				text += '<li class="arrow">' +
+				        '<a href="#venue" rel="' + place + '">' +
+				        '<span>'+place+'</span>' +
+				        '</a>' +
+				        '</li>';
+			}	
 			text += '</ul>';
 		}
+
+		if ( instance.type === 'day' ) {
+			for ( meal in instance.data.d[instance.venue][instance.day] ) {
+				text += '<li class="arrow">' +
+				        '<a href="#meal" rel="' + meal + '">' +
+				        meal_names[meal] +
+				        '</a>' +
+				        '</li>';
+			}	
+			text += '</ul>';
+		}
+
+		if (instance.type==='meal') {
+			text = '';
+			for ( station in instance.data.d[instance.venue][instance.day][instance.meal] ) {
+				text += '<h2>' + station + '</h2>';
+				items = instance.data.d[instance.venue][instance.day][instance.meal][station];
+				text += '<ul class="edgetoedge">';
+				for ( i = 0, l = items.length; i < l; i++ ) {
+					text += '<li class="arrow">' +
+					        '<a href="#nutrid" rel="' + items[i].n + '">' +
+					        items[i].i +
+					        '</a>' +
+					        '</li>';
+				}
+				text += '</ul>';
+			}
+		}
+
+		if ( instance.type === 'nutrition' ) {
+			text = "<p>No Nutritional Information</p>";
+			if ( instance.nutridata[ instance.nutrilink ] ) {
+				text = makeNutritionTemplate( instance );
+			} else {
+				$(instance.activator).addClass( LOADING_CLASSNAME );
+				// console.log('loading ajax');
+				$.ajax({
+					url: 'menus/' + instance.venue + instance.data.w + '.html',
+					async: false,
+					success: function(fullhtml) {
+						Eval( fullhtml.match( /aData=new Object\(\);([\s\S]*?)<\/script>/ )[1] );
+						if ( instance.nutridata[ instance.nutrilink ] ) {
+							text = makeNutritionTemplate( instance  );
+						}
+					},
+					error: function(r,s,e) {
+						// instance should go somewhere else higher, in much above too!
+						alert( CANNOT_LOAD_MESSAGE );
+					}   
+				});
+			}
+		}
+
+		return text;
+
 	}
-	return text;
 	
-};
-p.makeAll = function() {
-	return this.makeHead() + this.makeBody();
+	return makeHead() + makeBody();
 };
 
-
+return NumenuClass;
 // ==============================
 })();
